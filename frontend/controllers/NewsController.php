fe1a05b71c7128fde379e2bfe9eb45d602010469
Github-daijian <daijian102@hotmail.com>
2017-03-06 16:13:14 +0800

    提交News控制器

---
+++ frontend/controllers/NewsController.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: daijian
+ * Date: 2017/3/6
+ * Time: 16:11
+ */
+namespace frontend\controllers;
+
+use Yii;
+
+/**
+ * News controller
+ */
+class NewsController extends Controller
+{
+
+}
